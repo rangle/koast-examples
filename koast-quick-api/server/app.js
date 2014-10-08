2f098d43446dc7d57491e6790352331154ad8f89
Evan <evan@rangle.io>
2014-10-08 15:24:49 -0400

    Create koast-quick-api example

---
+++ koast-quick-api/server/app.js
@@ -0,0 +1,27 @@
+/*global require*/
+
+'use strict';
+
+var koast = require('koast');
+var appSettings = {
+  'app': {
+    'indexHtml': 'path:../client/index.html',
+    'portNumber': 8081,
+    'routes': [{
+      'route': '/api/v1',
+      'type': 'module',
+      'module': 'server/lib/api'
+    }]
+  },
+  'databases': [{
+    'host': '127.0.0.1',
+    'port': '27017',
+    'db': 'erg',
+    'schemas': './server/schemas.js',
+    'handle': '_'
+  }]
+};
+koast.configure({
+  appConfiguration: appSettings
+});
+koast.serve();
---
+++ koast-quick-api/server/schemas.js
@@ -0,0 +1,11 @@
+/* global exports */
+
+exports.schemas = [{
+  // Represents a task.
+  name: 'tasks',
+  properties: {
+    taskId: Number,
+    owner: String,
+    description: String
+  }
+}];
