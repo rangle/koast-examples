95d5c67222b2c8aac96bc8885a858571ff79d4df
Evan <evan@rangle.io>
2014-10-16 12:51:45 -0400

    Update examples to include todo apps
    - koast-todo - simple todo app with local auth
    - koast-todo-social-cookie - app using social auth / cookies, expects server
    and client to be on the same host
    - koast-todo-social-sessionless - uses token maintence, can have host and client
    on different servers. node server/app.js, http-server client/

---
+++ koast-todo-social-sessionless/server/app.js
@@ -0,0 +1,13 @@
+// step 1 - require koast
+var koast = require('koast');
+
+// step 2 - define basic configuration
+
+
+
+// room for improvement:
+// * ability to have multipul schema files
+// * ability to search directories and load up schemas, routes
+
+koast.configure();
+koast.serve();
